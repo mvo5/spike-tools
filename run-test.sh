6f55cf15c68a61f9aa2b5e41cd0a2991495b2b68
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2019-06-18 16:56:02 -0300

    run-test.sh: reduce vm ram to 256mb
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- run-test.sh
+++ run-test.sh
@@ -2,6 +2,6 @@
 
 sudo kvm \
   -bios /usr/share/ovmf/OVMF.fd \
-  -smp 2 -m 2000 -netdev user,id=mynet0,hostfwd=tcp::8022-:22,hostfwd=tcp::8090-:80 \
+  -smp 2 -m 256 -netdev user,id=mynet0,hostfwd=tcp::8022-:22,hostfwd=tcp::8090-:80 \
   -device virtio-net-pci,netdev=mynet0 \
   -drive file=pc.img,format=raw
